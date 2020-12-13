cc1d9eb0eca1f273e9af8a33e3bdeda8c2fb6813
keymaker13 <codecaptain13@gmail.com>
2020-12-12 23:21:54 -0600

    adding about me page

--- documents/javaFiles/bioinformatics/hw3/README.txt
+++
@@ -1,4 +0,0 @@
-Import the project into eclipse.
-Has two java programs.
-One is for A1 and A2.
-The other is for B2_c.
--- documents/javaFiles/dataStructures/currencyproject/Dime.java
+++
@@ -1,9 +0,0 @@
-package currency;
-
-public class Dime extends Currency {
-	
-	public Dime (){
-		super(10, "Dime", "US");
-	}
-
-}
