b4fee889e8ee540fea2739e268b1fe3bada2a854
keymaker13 <codecaptain13@gmail.com>
2020-12-12 23:14:52 -0600

    changed the about me page

--- documents/cFiles/operatingSystems/cs3733/assign4old/nqueens_library.h
+++
@@ -1,8 +0,0 @@
-#ifndef _nqueens_library_H_
-#define _nqueens_library_H_
-
-   int generate_n_queens_serial(int n, int print_flag);
-   void get_array_string(int *list,int n, char *s);
-   int next_permutation(int *list, int n);
-
-#endif
--- documents/cFiles/systemsProgramming/perl/variables.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-$number = 723; #integer
-$pi = 3.1415926; #numeric
-$avo = 6.02e23; #scientific notation
-$pet = 'dog'; #string
-$sign = "Beware of $pet\n"; #string with interpolation
-$curdir = `pwd`;	#command
-
