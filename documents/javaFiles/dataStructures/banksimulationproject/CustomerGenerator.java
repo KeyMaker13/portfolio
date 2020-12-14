4ef4d5f0bd7de315da4d36829236f36bd6a74384
keymaker13 <codecaptain13@gmail.com>
2020-12-14 13:15:38 -0600

    under construction

--- documents/javaFiles/dataStructures/banksimulationproject/CustomerGenerator.java
+++
@@ -1,9 +0,0 @@
-package bank;
-
-import java.util.Iterator;
-
-public interface CustomerGenerator {
-	public Iterator<Customer> iterator();
-	public void addCustomer(Customer c);
-	public int getNumberCustomers();
-}
--- mobile.js
+++
@@ -1,6 +0,0 @@
-var aboutMePage = "<h1> I am Ivan Capistran and I have been programming since I was 12. </h1> ";
-
-var documentsPage = "";
-
-
-
