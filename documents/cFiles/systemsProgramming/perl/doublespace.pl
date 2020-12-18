117fae4991995035801208bb83c6c6f693406f19
keymaker13 <codecaptain13@gmail.com>
2020-12-17 19:39:55 -0600

    changed it to another page.

--- documents/cFiles/systemsProgramming/perl/doublespace.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl -w
-open(INFILE,"file.txt");
-open(OUTFILE,"> file2.txt");
-while ($line = <INFILE>){
-	print OUTFILE $line, "\n";
-}
--- mobile.js
+++
@@ -1,6 +0,0 @@
-var aboutMePage = "<div> I am Ivan Capistran and I have been programming since I was 12.<br> I began programming in middle school. For more information please browse this site on desktop or laptop.  </div> ";
-
-var documentsPage = "<div>  To view my college files and source code please view ivancapistran.com on a desktop or laptop browser. </div>";
-
-
-
